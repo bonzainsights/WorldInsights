ef88f799669d8ad375bed53fdada4b91ffbb7d7f
Bijaya Acharya <93862729+achbj@users.noreply.github.com>
2026-07-31 15:03:09 +0200

    chore: clear repository contents

--- pipeline/worldinsights/__init__.py
+++
@@ -1,3 +0,0 @@
-"""WorldInsights canonical data and release tooling."""
-
-__version__ = "0.1.0"
--- tests/test_package.py
+++
@@ -1,5 +0,0 @@
-from worldinsights import __version__
-
-
-def test_package_version_is_defined() -> None:
-    assert __version__ == "0.1.0"
